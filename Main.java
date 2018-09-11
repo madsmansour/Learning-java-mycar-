96fe63c14ddc1eab90aa5d666e84fb4b3dd9d4d5
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-09-11 11:04:47 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+import static java.awt.Color.BLACK;
+import static java.awt.Color.BLUE;
+
+public class Main {
+    public static void main(String[]args) {
+        System.out.println("Hello world");
+        System.out.println("Mads");
+        System.out.println("Hvordan går det idag?");
+
+        Car myCar = new Car (25.5,"1BC32D", Color.BLUE, true);
+
+
+        Car sallyCar = new Car (13.5,"3D20BN",Color.BLACK,false);
+
+        System.out.println("My Car's license plate: " + myCar.licensePlate);
+        System.out.println("Sally's Car's license plate: " + sallyCar.licensePlate);
+        System.out.println(myCar.paintColor.toString());
+        myCar.changePaintColor(Color.RED);
+                System.out.println(myCar.paintColor.toString());
+
+    }
+}
