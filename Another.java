b3e43b5581d9854ac7f75c5251320fae863c3c43
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2018-09-13 17:27:35 +0200

    Add files via upload

---
+++ Another.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.util.Scanner;
+import java.util.Scanner;
+
+
+public class Another {
+    public static void main(String[] args) {
+        // System.out.println("Enter a Word:");
+        Scanner sc = new Scanner(System.in);
+        int userNumber = sc.nextInt();
+        System.out.println(userNumber);
+        double userNumber2 = sc.nextDouble();
+        System.out.println(userNumber2);
+
+    }
+       /* String userInput = sc.next();
+        String uppercased = userInput.toUpperCase();
+        System.out.println(userInput);
+        System.out.println(uppercased);
+
+        char firstcharacter = userInput.charAt(0);
+        System.out.println(firstcharacter);
+        System.out.println("Contains" + userInput.contains("Enter".toLowerCase()));*/
+    }
+
